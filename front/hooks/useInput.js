6a6fb50f7624158f5648be0954c384311c2ac97c
Mark Yoo <yjinhyuk@gmail.com>
2020-07-10 16:56:16 +0900

    Update: signup.js id, password, passwordcheck, nickname, term updated

--- front/hooks/useInput.js
+++ front/hooks/useInput.js
@@ -3,6 +3,7 @@ import { useState, useCallback } from "react";
 export default (initialValue = null) => {
   const [value, setValue] = useState(initialValue);
   const handler = useCallback((e) => {
+    console.log("now working");
     setValue(e.target.value);
   }, []);
   return [value, handler];
--- front/pages/signup.js
+++ front/pages/signup.js
@@ -1,15 +1,44 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import Head from "next/head";
-import { Form, Input } from "antd";
+import { Form, Input, Checkbox, Button } from "antd";
 import AppLayout from "../components/AppLayout";
 import useInput from "../hooks/useInput";
+import styled from "styled-components";
+
+const ErrorMessage = styled.div`
+  color: red;
+`;
 
 const Signup = () => {
   const [id, onChangeId] = useInput("");
   const [password, onChangePw] = useInput("");
   const [nickname, onChangeNick] = useInput("");
 
-  const onSubmit = useCallback(() => {}, []);
+  const [passwordCheck, setPasswordCheck] = useState("");
+  const [passwordError, setPasswordError] = useState(false);
+  const onPasswordCheck = useCallback(
+    (e) => {
+      setPasswordCheck(e.target.value);
+      setPasswordError(e.target.value !== password);
+    },
+    [password]
+  );
+
+  const [term, setTerm] = useState("");
+  const [termError, setTermError] = useState(false);
+  const onChangeTerm = useCallback((e) => {
+    setTerm(e.target.checked);
+    setTermError(false);
+  }, []);
+
+  const onSubmit = useCallback(() => {
+    if (password !== passwordCheck) {
+      return setPasswordError(true);
+    }
+    if (!term) {
+      return setTermError(true);
+    }
+  }, [password, passwordCheck, term]);
 
   return (
     <>
@@ -39,10 +68,38 @@ const Signup = () => {
             <br />
             <Input
               name="user-password"
+              type="password"
               value={password}
               required
               onChange={onChangePw}
             />
+          </div>
+          <div>
+            <label htmlFor="user-passwordCheck">비밀번호 확인</label>
+            <br />
+            <Input
+              name="user-passwordCheck"
+              type="password"
+              value={passwordCheck}
+              required
+              onChange={onPasswordCheck}
+            />
+            {passwordError && (
+              <ErrorMessage>비밀번호가 일치하지 않습니다.</ErrorMessage>
+            )}
+          </div>
+          <div>
+            <Checkbox name="user-term" checked={term} onChange={onChangeTerm}>
+              약관에 동의합니다.
+            </Checkbox>
+            {termError && (
+              <ErrorMessage>약관에 동의하셔야 합니다.</ErrorMessage>
+            )}
+          </div>
+          <div style={{ marginTop: 10 }}>
+            <Button type="primary" htmlType="submit">
+              가입하기
+            </Button>
           </div>
         </Form>
       </AppLayout>
