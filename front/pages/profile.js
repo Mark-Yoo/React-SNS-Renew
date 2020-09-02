69586ca63bfa3c6a871a44e8b71996a28ac5dfed
Mark Yoo <yjinhyuk@gmail.com>
2020-09-02 16:22:14 +0900

    Modified: click profile page does nothing when the user is not signed in

--- front/pages/profile.js
+++ front/pages/profile.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Head from 'next/head';
 import { useSelector } from 'react-redux';
 
+import Router from 'next/router';
 import AppLayout from '../components/AppLayout';
 import NicknameEditForm from '../components/NicknameEditForm';
 import FollowList from '../components/FollowList';
@@ -9,6 +10,15 @@ import FollowList from '../components/FollowList';
 const Profile = () => {
   const { me } = useSelector((state) => state.user);
 
+  useEffect(() => {
+    if (!(me && me.id)) {
+      Router.push('/');
+    }
+  }, [me && me.id]);
+
+  if (!me) {
+    return null;
+  }
   return (
     <>
       <Head>
