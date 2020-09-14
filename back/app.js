ac402abf0f1f1a40c07d0f5695d754a491bf7c25
Mark Yoo <yjinhyuk@gmail.com>
2020-09-14 16:51:45 +0900

    Modified: back/app.js

--- back/app.js
+++ back/app.js
@@ -1,11 +1,33 @@
 const express = require('express');
+const postRouter = require('./routes/post');
 
+const db = require('./models');
 const app = express();
+db.sequelize.sync()
+  .then(() => {
+    console.log('db 연결 성공');
+  })
+  .catch(console.error);
+
+// 순서대로 실행되어야 하므로 다음의 두 줄은 라우터보다 위에서 입력되어야 한다.
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.send('hello');
+  res.send('hello express');
 })
 
-app.listen(3306, () => {
+app.get('/posts', (req, res) => {
+  res.json([
+    {id: 1, content: 'hello'},
+    {id: 2, content: 'hello2'},
+    {id: 3, content: 'hello3'},
+  ]);
+});
+
+app.use('/post', postRouter);
+app.use('/user', userRouter);
+
+app.listen(3065, () => {
   console.log('서버 실행 중')
 })
